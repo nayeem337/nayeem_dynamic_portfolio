62ec5ce0767b93d89b51c7f62ae1e8ac3febe26c
Jannatul Nayeem <123932013+nayeem337@users.noreply.github.com>
2023-08-02 15:22:38 +0600

    first commit

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,5 @@
+<div class="iconeArea bg-secondar">
+    <a href="https://www.facebook.com/iamarifhossen"><i class="fab fa-facebook-f"></i></a>
+    <a href=""><i class="fab fa-twitter"></i></a>
+    <a href="linkedin.com/in/md-arif-hossen-0505a6216"><i class="fab fa-linkedin"></i></a>
+</div>
